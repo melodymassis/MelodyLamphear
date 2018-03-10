a4bc9235afe5b68146447b3643cc108a9ccfd482
MelodyLamphear <melodymassis@gmail.com>
2018-03-10 01:22:32 -0800

    Complete Analysis

---
+++ Tweet_Sentiment/localenv.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "RoszS1qqFEepcABgv2SgsMA4j"
+consumer_secret = "e9zOIYEvR8TFRDaIcXn97z9aRPwSarPpEBaKO8T1ZCUqzVLXq7"
+access_token = "968344995941019650-qba5I2Q7gFDu5nIB36sEEclJjmIrKId"
+access_token_secret = "2ymVdTEHtjlA2tNHnyVHTqkr44hcGHXlIoR8OIxRmaUqg"
